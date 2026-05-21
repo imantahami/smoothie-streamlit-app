474cdc57f5432bb74c4da05c706304fc04bc6cad
iman tahami <117601702+imantahami@users.noreply.github.com>
2026-05-21 21:43:27 +0200

    Refactor smoothie order functionality and remove nutrition display
    Removed the section displaying nutrition information for watermelon and streamlined the smoothie ordering process.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,17 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+
         ingredients_string += fruit_chosen + ' '
+
+        smoothiefroot_response = requests.get(
+            f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}"
+        )
+
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
 
     my_insert_stmt = f"""
         insert into smoothies.public.orders
@@ -46,18 +56,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-# new section to display smoothie request nutrition information
-
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-
-st.text(smoothiefroot_response)
-
-st.text(smoothiefroot_response.json())
-
-st.dataframe(
-    data=smoothiefroot_response.json(),
-    use_container_width=True
-)
