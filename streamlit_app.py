09f847ef0dd273341f98c3b4dfb69d2572f8b323
iman tahami <117601702+imantahami@users.noreply.github.com>
2026-05-21 21:53:54 +0200

    Add nutrition information subheader for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ if ingredients_list:
 
         ingredients_string += fruit_chosen + ' '
 
+        st.subheader(fruit_chosen + ' Nutrition Information')
+
         smoothiefroot_response = requests.get(
             f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}"
         )
